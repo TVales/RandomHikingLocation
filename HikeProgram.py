fc5ab7d038882631cc7771129aa56bfebef3c50d
Tvales <timv96@vt.edu>
2017-12-08 02:37:33 -0500

    First commit of random hiking program

---
+++ HikeProgram.py
@@ -0,0 +1,26 @@
+#Enter places to hike and gives you a random place you should hike. Decision-Maker program. Random
+
+import random #imports the random module
+
+placeToHike = [] #array that stores all the places to hike
+
+while True:
+    print('Enter the name of the trail you would like to hike or enter nothing to stop')
+    place = input()
+    if place == '':
+        break
+    placeToHike = placeToHike + [place]
+
+print('There are %d places you wanted to hike' % len(placeToHike)) #formatted output
+print('Here are the places you wanted to hike')
+for name in placeToHike: #for loop to traverse places
+    print(' ' + name)
+
+rand = random.randint(1, len(placeToHike))
+randomPlace = placeToHike[rand]
+
+print('You should hike ' + randomPlace)
+
+#ADD MORE functionality when you learn web scraping
+#when chosen random place to hike give area and location of that hike
+#give info for that hike like location, distance (roundtrip), popularity, distance from current location
