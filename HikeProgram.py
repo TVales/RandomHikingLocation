dfe4926fd55cb110be058ce92fd5c42ba494955e
Tvales <timv96@vt.edu>
2018-02-25 19:29:06 -0500

    openWeb Deleted because it was the same as FixWebBrowser

--- HikeProgram.py
+++ HikeProgram.py
@@ -1,13 +1,11 @@
-#Enter places to hike in JNF (Jeffersona Natl Forest) area and gives you a random place you should hike. Decision-Maker program. Random
+#Enter places to hike and gives you a random place you should hike. Decision-Maker program. Random
 
 import random #imports the random module
-import webbrowser #allows for opening of web browser
-import time
 
 placeToHike = [] #array that stores all the places to hike
 
 while True:
-    print('Enter the name of the trail you would like to hike in the JNF (Jefferson Natl Forest) area with no spaces or enter nothing to stop')
+    print('Enter the name of the trail you would like to hike or enter nothing to stop')
     place = input()
     if place == '':
         break
@@ -18,11 +16,7 @@ print('Here are the places you wanted to hike')
 for name in placeToHike: #for loop to traverse places
     print(' ' + name)
 
-rand = random.randint(1, len(placeToHike) - 1)
+rand = random.randint(1, len(placeToHike))
 randomPlace = placeToHike[rand]
 
 print('You should hike ' + randomPlace)
-
-time.sleep(3)
-
-webbrowser.open('https://www.hikingupward.com/JNF/' + randomPlace + '/') #DOES NOT WORK. ERROR ON WEBPAGE. CANNOT FIND. 
