d64a0648c8b98899badf0fdb41bc3ec575e28f54
Tvales <timv96@vt.edu>
2018-02-25 21:20:29 -0500

    Fixed format so hiking places can be opened by web browser to Hiking Upwards website

--- HikeProgram.py
+++ HikeProgram.py
@@ -3,26 +3,39 @@
 import random #imports the random module
 import webbrowser #allows for opening of web browser
 import time
+import string
 
 placeToHike = [] #array that stores all the places to hike
 
 while True:
-    print('Enter the name of the trail you would like to hike with no spaces or enter nothing to stop')
+    print('Enter the name of the trail you would like to hike in the NJF or enter nothing to stop')
     place = input()
+
     if place == '':
         break
     placeToHike = placeToHike + [place]
 
 print('There are %d places you wanted to hike' % len(placeToHike)) #formatted output
 print('Here are the places you wanted to hike')
+
 for name in placeToHike: #for loop to traverse places
-    print(' ' + name)
+    print(name)
 
 rand = random.randint(1, len(placeToHike) - 1)
 randomPlace = placeToHike[rand]
 
-print('You should hike ' + randomPlace)
+allCaps = string.capwords(randomPlace) #if first letter of word is lowercase it's changed to upper case. Fit website format
+noSpaces = allCaps.replace(" ","") #replaces all whitespaces with no space. Fit website format
 
-time.sleep(5)
+#special cases on website. Add more special cases here
+if noSpaces == 'McafeesKnob':
+    noSpaces = 'McAfeeKnob'
 
-webbrowser.open('https://www.hikingupward.com/JNF/' + randomPlace) #Does not work. Fix this portion to move on. Can be problem with website
+if noSpaces == 'TinkerCliffs':
+    noSpaces = 'TinkerCliffsAndyLayneTrail'
+
+print('You should hike ' + allCaps)
+
+time.sleep(4)
+
+webbrowser.open('https://www.hikingupward.com/JNF/' + noSpaces) #Does not work. Fix this portion to move on. Can be problem with website
