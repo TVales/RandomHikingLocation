4e128afee5835019a0a26c0b71da7c8667cd3b55
Tvales <timv96@vt.edu>
2018-02-25 21:38:23 -0500

    Fixed Web browser functionality. Opens to Hiking Upward with given random Place

--- HikeProgram.py
+++ HikeProgram.py
@@ -1,21 +1,14 @@
 #Enter places to hike in NJF area and gives you a random place you should hike. Decision-Maker program. Random
 
 import random #imports the random module
-<<<<<<< HEAD
-=======
 import webbrowser #allows for opening of web browser
 import time
 import string
->>>>>>> FixWebBrowser
 
 placeToHike = [] #array that stores all the places to hike
 
 while True:
-<<<<<<< HEAD
-    print('Enter the name of the trail you would like to hike or enter nothing to stop')
-=======
     print('Enter the name of the trail you would like to hike in the NJF or enter nothing to stop')
->>>>>>> FixWebBrowser
     place = input()
 
     if place == '':
@@ -31,9 +24,6 @@ for name in placeToHike: #for loop to traverse places
 rand = random.randint(1, len(placeToHike))
 randomPlace = placeToHike[rand]
 
-<<<<<<< HEAD
-print('You should hike ' + randomPlace)
-=======
 allCaps = string.capwords(randomPlace) #if first letter of word is lowercase it's changed to upper case. Fit website format
 noSpaces = allCaps.replace(" ","") #replaces all whitespaces with no space. Fit website format
 
@@ -49,4 +39,3 @@ print('You should hike ' + allCaps)
 time.sleep(4)
 
 webbrowser.open('https://www.hikingupward.com/JNF/' + noSpaces) #Does not work. Fix this portion to move on. Can be problem with website
->>>>>>> FixWebBrowser
